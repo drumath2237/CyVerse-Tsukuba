ba507684eb93be38b65f60047940de89bb95b03b
drumath <2237k.drumath@gmail.com>
2022-06-03 18:15:17 +0900

    :sparkles: playbackようのスクリプトを追加

---
+++ Assets/GeospatialTsukubaAISTTestbed/Scenes/SessionPlaybackManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.XR.ARCore;
+using UnityEngine.XR.ARFoundation;
+
+namespace GeospatialTsukuba
+{
+    public class SessionPlaybackManager : MonoBehaviour
+    {
+        [SerializeField]
+        private ARSession arSession;
+
+        [SerializeField]
+        private bool playbackOnAwake;
+
+        [SerializeField]
+        private string mp4Filename = "arcore-session.mp4";
+
+        private void Awake()
+        {
+            if (playbackOnAwake)
+            {
+                PlaybackSession();
+            }
+        }
+
+
+        public void PlaybackSession()
+        {
+#if UNITY_ANDROID
+            if (!arSession || arSession.subsystem is not ARCoreSessionSubsystem subsystem)
+            {
+                Debug.LogError("ar session is not compatible with ar core session");
+                return;
+            }
+
+            if (subsystem.playbackStatus == ArPlaybackStatus.Finished)
+            {
+                StopPlayback();
+            }
+
+
+            var mp4Path = Path.Combine(Application.persistentDataPath, mp4Filename);
+            if (!File.Exists(mp4Path))
+            {
+                return;
+            }
+
+            subsystem.StartPlayback(mp4Path);
+#endif
+        }
+
+        public void StopPlayback()
+        {
+#if UNITY_ANDROID
+            if (!arSession || arSession.subsystem is not ARCoreSessionSubsystem subsystem)
+            {
+                Debug.LogError("ar session is not compatible with ar core session");
+                return;
+            }
+
+            subsystem.StopPlayback();
+#endif
+        }
+    }
+}
