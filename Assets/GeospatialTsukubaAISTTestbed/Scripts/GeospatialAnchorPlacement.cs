b6a4a87535049da7289a882c7b70d55f27a2da47
drumath <2237k.drumath@gmail.com>
2022-06-03 18:58:44 +0900

    :sparkles: geospatial apiの動作を確認

---
+++ Assets/GeospatialTsukubaAISTTestbed/Scripts/GeospatialAnchorPlacement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Google.XR.ARCoreExtensions;
+using TMPro;
+using UnityEngine;
+
+namespace GeospatialTsukuba
+{
+    public class GeospatialAnchorPlacement : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI logText;
+
+        [SerializeField]
+        private AREarthManager earthManager;
+
+        
+
+        private void Update()
+        {
+            if (!logText)
+            {
+                return;
+            }
+
+            var pose = earthManager.CameraGeospatialPose;
+            var earthState = earthManager.EarthState;
+
+            logText.text = $"Tracking State:{earthState}\nlat,lon:{pose.Latitude}, {pose.Longitude}";
+        }
+    }
+}
