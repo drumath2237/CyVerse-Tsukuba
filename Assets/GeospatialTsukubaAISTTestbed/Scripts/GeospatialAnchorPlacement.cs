2be9ef68e9c5f18bd96497390418631f65681149
drumath <2237k.drumath@gmail.com>
2022-06-03 19:42:15 +0900

    :tada: 広場のデジタルツインを位置合わせ成功

--- Assets/GeospatialTsukubaAISTTestbed/Scripts/GeospatialAnchorPlacement.cs
+++ Assets/GeospatialTsukubaAISTTestbed/Scripts/GeospatialAnchorPlacement.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using Google.XR.ARCoreExtensions;
+﻿using Google.XR.ARCoreExtensions;
 using TMPro;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 namespace GeospatialTsukuba
 {
@@ -13,7 +13,17 @@ namespace GeospatialTsukuba
         [SerializeField]
         private AREarthManager earthManager;
 
-        
+        [SerializeField]
+        private ARAnchorManager arAnchorManager;
+
+        [SerializeField]
+        private GameObject anchorObject = null;
+
+        private Transform anchorTransform = null;
+
+        [SerializeField]
+        private GameObject testmodel;
+
 
         private void Update()
         {
@@ -26,6 +36,34 @@ namespace GeospatialTsukuba
             var earthState = earthManager.EarthState;
 
             logText.text = $"Tracking State:{earthState}\nlat,lon:{pose.Latitude}, {pose.Longitude}";
+            if (anchorTransform)
+            {
+                logText.text += $"anchor pose:{anchorTransform.position}";
+                testmodel.transform.rotation = anchorTransform.rotation;
+            }
+        }
+
+        public void PlaceAnchor()
+        {
+            if (!arAnchorManager)
+            {
+                return;
+            }
+
+            var anchor = arAnchorManager.AddAnchor(
+                36.08219375,
+                140.1133591,
+                68.958,
+                Quaternion.identity
+            );
+
+            if (!anchorObject)
+            {
+                return;
+            }
+
+            var obj = Instantiate(anchorObject, anchor.transform);
+            anchorTransform = obj.transform;
         }
     }
 }
